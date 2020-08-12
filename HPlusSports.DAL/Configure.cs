6b8fdc823d99a768849d1ba2cb0c84f86ffc2634
Rich Goforth <rcgoforth@gmail.com>
2020-08-12 14:06:33 -0400

    Initial Copy

---
+++ HPlusSports.DAL/Configure.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HPlusSports.Models;
+
+namespace HPlusSports.DAL
+{
+    public static class Configure
+    {
+        public static void ConfigureServices(IServiceCollection services, string connectionString)
+        {
+            //Context lifetime defaults to "scoped"
+            services
+                 .AddDbContext<HPlusSportsContext>(options => options.UseSqlServer(connectionString));
+
+            services
+                .AddScoped<IOrderRepository, OrderRepository>()
+                .AddScoped<IRepository<Customer>, AtomicRepository<Customer>>()
+                .AddScoped<IRepository<Product>, AtomicRepository<Product>>()
+                .AddScoped<ISalesPersonRepository, SalesPersonRepository>()
+                .AddScoped<ITrackingRepository<SalesGroup>, TrackingRepository<SalesGroup>>();
+        }
+    }
+}
